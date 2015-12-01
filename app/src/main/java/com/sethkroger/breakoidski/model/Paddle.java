238905f9184ff3e1407a4039aecd0b64762f34c3
Seth Kroger <seth.kroger@gmail.com>
2015-11-30 22:54:23 -0800

    Add paddle with slides horizontally to touch to GameView.

--- app/src/main/java/com/sethkroger/breakoidski/model/Paddle.java
+++ app/src/main/java/com/sethkroger/breakoidski/model/Paddle.java
@@ -1,16 +1,71 @@
 package com.sethkroger.breakoidski.model;
 
+import android.graphics.Canvas;
 import android.graphics.Matrix;
-import android.graphics.Rect;
+import android.graphics.Paint;
+import android.graphics.RectF;
 
 /**
  * Breakout paddle game object.
  */
 public class Paddle {
-    public static final int PADDLE_WIDTH = 50;
-    public static final int PADDLE_HEIGHT = 5;
+    public static final float HALF_PADDLE_WIDTH = 25.0f;
+    public static final float HALF_PADDLE_HEIGHT = 2.5f;
+    public static final int PADDLE_COLOR = 0xff336699;
 
     private Matrix mMatrix;
-    private Rect mPaddleShape;
+    private float mYpos;
+    private RectF mPaddleShape;
+    private Paint mPaint;
 
+    /**
+     * Creates a Paddle with default size and styling.
+     */
+    public Paddle() {
+        mMatrix = new Matrix();
+        mPaddleShape = new RectF(-HALF_PADDLE_WIDTH, -HALF_PADDLE_HEIGHT, HALF_PADDLE_WIDTH, HALF_PADDLE_HEIGHT);
+        mPaint = new Paint();
+        mPaint.setColor(PADDLE_COLOR);
+        mPaint.setStyle(Paint.Style.FILL);
+    }
+
+    /**
+     * Put the paddle at the screen position (x,y)
+     *
+     * @param x x coordinate in pixels
+     * @param y y coordinate in pixels
+     */
+    public void setPosition(int x, int y) {
+        mMatrix.setTranslate(x, y);
+        mYpos = y;
+    }
+
+    /**
+     * Set the paddle horizontal position of the paddle on the game view.
+     * This is meant as the primary means of moving the paddle object during the game
+     * since the paddle object is meant to side horizontally only.
+     *
+     */
+    public void setPosition(float x) {
+        mMatrix.setTranslate(x, mYpos);
+    }
+
+    /**
+     * Performs the periodic update as required by the game engine.
+     *
+     */
+    public void update() {
+
+    }
+
+    /**
+     * Draw the paddle at its current location.
+     *
+     * @param canvas The canvas object to draw to.
+     */
+    public void draw(Canvas canvas) {
+        RectF paddle = new RectF();
+        mMatrix.mapRect(paddle, mPaddleShape);
+        canvas.drawRect(paddle, mPaint);
+    }
 }
--- app/src/main/java/com/sethkroger/breakoidski/ui/GameView.java
+++ app/src/main/java/com/sethkroger/breakoidski/ui/GameView.java
@@ -4,15 +4,18 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import com.sethkroger.breakoidski.model.Paddle;
 
 /**
  * The Custom view that holds the main game canvas
  */
 public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private static final float PADDLE_TRACK_SCREEN_FRACTION = 0.8f;
 
     private Context mContext;
     private SurfaceHolder mHolder;
@@ -20,12 +23,16 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     private float mTouchX = 0.0f;
     private float mTouchY = 0.0f;
 
+    private Paddle mPaddle;
+
 
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
         mContext = context;
         mHolder = getHolder();
         mHolder.addCallback(this);
+
+        mPaddle = new Paddle();
     }
 
     @Override
@@ -33,22 +40,39 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
         if (event.getActionMasked() == MotionEvent.ACTION_MOVE || event.getActionMasked() == MotionEvent.ACTION_DOWN) {
             mTouchX = event.getX();
             mTouchY = event.getY();
-            drawDot();
+            mPaddle.setPosition(mTouchX);
+            draw();
             return true;
         }
         return false;
     }
 
-    private void drawDot() {
+    /**
+     * Blanks the view and redraws the components.
+     */
+    public void draw() {
         Canvas canvas = mHolder.lockCanvas();
+
+        // redraw background to erase previous canvas
+        canvas.drawARGB(255, 0, 0, 0);
+
+        mPaddle.draw(canvas);
+
+        mHolder.unlockCanvasAndPost(canvas);
+    }
+
+    /**
+     * Draw a light blue dot at the tracked touch position.
+     * Used mainly for debugging purposes to highlight where the input touch coordinates are
+     *
+     * @param canvas Canvas object to draw the dot on/
+     */
+    private void drawDot(Canvas canvas) {
         Paint paint = new Paint();
         paint.setStrokeWidth(5.0f);
         paint.setColor(0xff336699);
 
-        canvas.drawARGB(255, 0, 0, 0);
         canvas.drawCircle(mTouchX, mTouchY, 20.0f, paint);
-
-        mHolder.unlockCanvasAndPost(canvas);
     }
 
     @Override
@@ -58,7 +82,7 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
+        mPaddle.setPosition((int) mTouchX, (int) (height * PADDLE_TRACK_SCREEN_FRACTION));
     }
 
     @Override
